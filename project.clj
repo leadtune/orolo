0bfb7a901473b05370b36ce15899e8f5f057ddf2
Tim Harper <timcharper@gmail.com>
2010-07-19 16:38:54 -0600

    upgrade swank, and switch it to a dev-dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :dependencies [[org.clojure/clojure
                   "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib
-                  "1.2.0-SNAPSHOT"]
-                 [org.clojure/swank-clojure
-                  "1.0"]]
+                  "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
   :namespaces [orolo.periodically])
